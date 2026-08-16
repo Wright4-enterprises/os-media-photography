50df853cc37253c4eb6142593d9ec6547c0ad975
wright4enterprises <wright4enterprises@gmail.com>
2026-08-17 01:36:36 +0800

    Update shoots-list.js

--- netlify/functions/shoots-list.js
+++ netlify/functions/shoots-list.js
@@ -7,15 +7,18 @@ export default async (req, context) => {
   const manifest = manifestRaw ? JSON.parse(manifestRaw) : [];
 
   // Turn stored photo keys into URLs the gallery page can use as <img src>
- const shoots = manifest.map(shoot => ({
+const shoots = manifest.map(shoot => ({
   id: shoot.id,
   title: shoot.title,
   date: shoot.date,
   coverPhoto: shoot.coverPhoto || shoot.photoKeys[0],
   photos: shoot.photoKeys.map(key => `/.netlify/functions/photo?key=${encodeURIComponent(key)}`),
   photoKeys: shoot.photoKeys,
-  featuredPhotos: shoot.featuredPhotos || []
+  featuredPhotos: shoot.featuredPhotos || [],
+  hidden: shoot.hidden || false,
+  archived: shoot.archived || false
 }));
+
 
   return new Response(JSON.stringify({ shoots }), {
     status: 200,
